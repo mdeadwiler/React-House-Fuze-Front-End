dcc1cd710e3b1e016197d30860cc304e60247ffc
MDeadwiler <mdeadwiler@email.com>
2024-10-26 18:59:09 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import "./App.css"
+const App = () => {
+  return (
+    <>
+  <h1>House Fuze</h1>
+  <p>Connecting Your Home Needs with Trusted Pros</p>
+  </>
+  )
+  
+};
+
+export default App;
