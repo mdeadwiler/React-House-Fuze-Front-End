3e8f9cc2150240daf8da6dcb44b10b08ad25b443
MDeadwiler <mdeadwiler@email.com>
2024-10-26 23:17:39 -0400

    here is the template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // src/App.jsx
-import "./App.css"
+
+
+import "./App.css";
+
 const App = () => {
   return (
     <>
-  <h1>House Fuze</h1>
-  <h2>Marquise Deadwiler</h2>
-  <p>Connecting Your Home Needs with Trusted Pros</p>
-  </>
-  )
-  
+
+      <h1>House Fuze</h1>
+      <h2>Marquise Deadwiler</h2>
+      <p>Connecting Your Home Needs with Trusted Pros</p>
+    </>
+  );
 };
 
 export default App;
