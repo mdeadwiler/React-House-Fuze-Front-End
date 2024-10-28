96ec2c16024034c79baf2d4f35542663725784dd
George-25 <georgemallory26@gmail.com>
2024-10-28 15:50:28 -0400

    updated branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 // src/App.jsx
-<<<<<<< HEAD
 import LoginForm from "./components/LoginForm/LoginForm";
 
 import "./App.css"
-=======
-import NavBar from "./components/NavBar/navBar.jsx";
+// import NavBar from "./components/NavBar/navBar.jsx";
 import "./App.css";
 
 
 
 
->>>>>>> 0e7c0881b4694de202832135d6f4393c21a27b00
 const App = () => {
   
   return (
