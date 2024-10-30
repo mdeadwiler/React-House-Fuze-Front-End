99167d3927620f78a6224ab612b032b01c7e1bf7
MDeadwiler <mdeadwiler@email.com>
2024-10-30 10:13:43 -0400

    signupform

--- src/components/SignupForm/signupForm.jsx
+++ src/components/SignupForm/signupForm.jsx
@@ -1,147 +1,181 @@
 // Marquise
 
 import { useState } from "react";
-import {Link, useNavigate } from "react-router-dom";
-import { SignupForm } from "../SignupForm/SignupForm";
-
-
+import { Link, useNavigate } from "react-router-dom";
+//import { SignupForm } from "../SignupForm/SignupForm";
 
 function SignupForm({ setUser }) {
-   const [string, setString] = useState("");
-   const [formData, setFormData] = useState({
+  const [string, setString] = useState("");
+  const [formData, setFormData] = useState({
     username: "",
-    hashedPassword: "",
+    password: "",
+    passwordConf: "",
     email: "",
     firstName: "",
     lastName: "",
     isHomeOwner: "",
     contractorCompany: "",
     contractorCategory: "",
-}) 
-const navigate = useNavigate();
-const updateMessage = (string) => {
-   setString(string);
-};
-   const handleChange = (event) => {
-      const user = event.target[{ username, hashedPassword, email, firstName, lastName, isHomeOwner, contractorCompany, contractorCategory }]
+  });
 
-      setFormData(( prevFormData ) => ({
-         ...prevFormData,
-         [user]: user,
-      }));
-   };
-const handleSubmit = async (event) => {
-   event.preventDefault();
+  const navigate = useNavigate();
 
-   if (formData.hashedPassword !== formData.hashedPasswordConf) {
+  const updateMessage = (string) => {
+    setString(string);
+  };
+
+  const handleChange = (event) => {
+     const user =
+       event.target[
+         {
+           username,
+           password,
+           email,
+           firstName,
+           lastName,
+           //isHomeOwner,
+           contractorCompany,
+           contractorCategory,
+         }
+       ];
+    const { name, value } = event.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (formData.password !== formData.passwordConf) {
       updateMessage("This password does not match, try again!");
       return;
-   }
-   try {
+    }
+    try {
       const userRes = await SignupForm(formData);
       setUser(userRes.user);
-      navigate("/")
-   } catch (error) {
+      navigate("/");
+    } catch (error) {
       updateMessage(error.message);
-   }
-};
-const isFormInvalid = () => {
-   return !(username && hashedPassword === hashedPasswordConf);
-};
-const { username, hashedPassword, hashedPasswordConf} = formData;
+    }
+  };
 
-return (
-   <main>
+  const isFormInvalid = () => {
+    return !(username && password === passwordConf);
+  };
+
+  const {
+    username,
+    password,
+    passwordConf,
+    email,
+    firstName,
+    lastName,
+    contractorCompany,
+    contractorCategory,
+  } = formData;
+
+  return (
+    <main>
       <h1>Join us</h1>
       <p>{string}</p>
       <form onSubmit={handleSubmit}>
-         <div>
-            <label htmlFor="username">Username</label>
-            <input
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
             type="text"
             id="username"
             value={username}
             name="username"
-            onChange={handleChange} />
-            </div>
-            <div>
-            <label htmlFor="email">Email</label>
-            <input
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
             type="email"
             id="email"
             value={email}
             name="email"
-            onChange={handleChange} />
-            </div>
-            <div>
-            <label htmlFor="firstName">First Name</label>
-            <input
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
             type="text"
             id="username"
             value={firstName}
             name="firstName"
-            onChange={handleChange} />
-            </div>
-            <div>
-            <label htmlFor="lastName">Last Name</label>
-            <input
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
             type="text"
             id="username"
             value={lastName}
             name="lastName"
-            onChange={handleChange} />
-            </div>
-            <div>
-            <label htmlFor="contractorCategory">Contractor Category</label>
-            <input
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="contractorCategory">Contractor Category</label>
+          <input
             type="text"
             id="contractorCategory"
             value={contractorCategory}
             name="contractorCategory"
-            onChange={handleChange} />
-            </div>
-            <div>
-            <label htmlFor="hashedPassword">Password</label>
-            <input
-            type="hashedPassword"
-            id="hashedPassword"
-            value={hashedPassword}
-            name="hashedPassword"
-            onChange={handleChange} />
-            </div>
-            <div>
-          <label htmlFor="confirm">Confirm Password:</label>
-          <input
-            type="hashedPassword"
-            id="confirm"
-            value={hashedPasswordConf}
-            name="hashedPasswordConf"
             onChange={handleChange}
           />
         </div>
-            <div>
-            <label htmlFor="contractorCompany">Contractor Company</label>
-            <input
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="passwordConf">Confirm Password:</label>
+          <input
+            type="password"
+            id="passwordConf"
+            value={passwordConf}
+            name="passwordConf"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="contractorCompany">Contractor Company</label>
+          <input
             type="title"
             id="contractorCompany"
-            value={contractoCompany}
+            value={contractorCompany}
             name="contractorCompany"
-            onChange={handleChange} />
-            </div>
-            <div>
+            onChange={handleChange}
+          />
+        </div>
+        <div>
           <button disabled={isFormInvalid()}>Sign Up</button>
           <Link to="/">
             <button>Cancel</button>
           </Link>
         </div>
-            
-      {/* see about !isHomeOwner */}
-      </form>
-   </main>
-)
 
+        {/* see about !isHomeOwner */}
+      </form>
+    </main>
+  );
 }
 /*if (!isHomeOwner){
    userData.contractorCompany = contractorCompany
    userData.contractorCategory = contractorCategory
   }*/
+export default SignupForm;
