c9a90a48f3bd6a3188b4678ac2e315178525d0de
MDeadwiler <mdeadwiler@email.com>
2024-10-30 10:57:24 -0400

    updated-with-bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-
 // src/App.jsx
-
-import NavBar from "./components/NavBar/navBar.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
 import { createContext, useState } from "react";
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PostJob from './components/PostJob';
 import JobDetails from './components/JobDetails';
 import "./App.css";
-Create context for authenticated user
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
 const [user, setUser] = useState(null); // State for the authenticated user
 
-//   // Dummy function to handle signout (for demonstration purposes)
-//   const handleSignout = () => {
-//     setUser(null);
-//   };
+
+  const handleSignout = () => {
+    setUser(null);
+  };
 
   return (
     <AuthedUserContext.Provider value={{ user }}>
@@ -40,5 +36,6 @@ const [user, setUser] = useState(null); // State for the authenticated user
       //  <Comments jobId={12} comments={commentArray}/>
       }
     </AuthedUserContext.Provider>
-
+  )
+};
 export default App;
